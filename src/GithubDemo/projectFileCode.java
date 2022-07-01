59041db2c7ec629f05ad8d4857d7c7d0daceabc4
HP <HP@LAPTOP-BU5UP52J>
2022-07-01 22:43:08 +0530

    project is now commited

---
+++ src/GithubDemo/projectFileCode.java
@@ -0,0 +1,24 @@
+package GithubDemo;
+
+public class projectFileCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("hello there");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
